81c059291f3162e0ace66920bbabb72a5af00a2d
AlYaqdhan Al Maawali <almaa009@umn.edu>
2023-12-25 13:12:08 +0400

    fixed styling

--- Aviani/aviani-app/src/components/hero.tsx
+++ Aviani/aviani-app/src/components/hero.tsx
@@ -1,14 +1,30 @@
 import videoBg from "../assets/dunes.mp4";
-import "./hero.css";
+import "../index.css";
 
 function Hero() {
   return (
-    <div className="hero">
-      <div className="overlay"></div>
-      <video src={videoBg} autoPlay loop muted />
-      <div className="content">
-        <h1>Aviani</h1>
-        <p>visuals</p>
+    <div className="relative w-screen h-96">
+      {/* Video */}
+      <video
+        className="w-full h-full object-cover"
+        src={videoBg}
+        autoPlay
+        loop
+        muted
+      />
+
+      {/* Overlay */}
+      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+        {/* Text */}
+        <div className="text-center text-white">
+          <h1 className="text-one">Aviani visuals</h1>
+          <p className="text-three">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
+            sunt porro quo ut qui repellendus, voluptatum soluta consequuntur
+            asperiores omnis? Nostrum nihil ipsa sunt, cum dolor sapiente id
+            distinctio accusantium?
+          </p>
+        </div>
       </div>
     </div>
   );
--- Aviani/aviani-app/tailwind.config.js
+++ Aviani/aviani-app/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "one":"#88AB8E",
+        "two":"#AFC8AD",
+        "three":"#EEE7DA",
+        "four":"#F2F1EB",
+      }
+    },
   },
   plugins: [],
 }
