4ddfc6aa9627fa3971df1cbfc2fe90535108a489
OVirusx <azharx6@gmail.com>
2024-04-13 21:51:04 +0400

    commented out bootstrao for now. needs a fix.

--- FindYourDreamCar/FindYourDreamCar/src/main.tsx
+++ FindYourDreamCar/FindYourDreamCar/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "bootstrap"; // probably need to be fixed
+// import "bootstrap"; // probably need to be fixed
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
